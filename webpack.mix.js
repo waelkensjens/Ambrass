650353b703f8352f32af7a7de55c88b87f77b3ba
jens <waelkens.jens@gmail.com>
2022-02-28 13:37:10 +0100

    :wrench: last configuration to get this working

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,21 @@
-const mix = require('laravel-mix');
+const path = require("path");
+const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+// Resolve Ziggy
+mix.alias({
+    ziggy: path.resolve("vendor/tightenco/ziggy/dist/vue"),
+});
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [require("tailwindcss")]
-);
+// Build files
+mix.js("resources/js/app.js", "public/js")
+    .vue({ version: 3 })
+    .webpackConfig({
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "resources/js"),
+            },
+        },
+    })
+    .extract()
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .version();
